8eaf53c92a5c74744a64ca0d6d94e4a8530a6df9
Ilya Sytchev <isytchev@hsph.harvard.edu>
2014-09-05 11:52:23 -0400

    Initial commit

---
+++ slurm_nodes.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""Prints a comma separated list of SLURM compute nodes that have exessive CPU load.
+Output is suitable for using as value for -x option.
+
+Usage: $ python slurm_nodes.py
+
+Depends on scontrol command.
+
+"""
+import subprocess
+import sys
+
+
+def stop_err(msg, returncode=1):
+    sys.stderr.write(msg)
+    sys.exit(returncode)
+
+
+cmd = 'scontrol -o show node'
+
+try:
+    output = subprocess.check_output(cmd.split())
+except subprocess.CalledProcessError as e:
+    stop_err("Error executing '{}'".format(cmd), e.returncode)
+
+node_list = []
+for node_str in output.splitlines():
+    for param_str in node_str.split():
+        param = param_str.split('=')
+        try:
+            if param[0] == 'NodeName':
+                node_name = param[1]
+            elif param[0] == 'CPUTot':
+                cpu_total = int(param[1])
+            elif param[0] == 'CPULoad':
+                cpu_load = float(param[1])
+        except ValueError:
+            break
+    if cpu_load > cpu_total * 1.05:
+        node_list.append(node_name)
+
+print ','.join(node_list)
